0d898cbcc267681cbce48a02c875257b83842ece
frodeheg <frode.heggelund@gmail.com>
2022-05-03 01:11:09 +0200

    Finalized support for version 1.0 of the app * Added flow cards and actions * Added the rest of the interesting capabilities

--- drivers/hiax-connected-200/device.js
+++ drivers/hiax-connected-200/device.js
@@ -5,6 +5,16 @@ const { OAuth2Device } = require('homey-oauth2app');
 
 class MyHoiaxDevice extends OAuth2Device {
 
+  async setHeaterState(deviceId) {
+    // Value 0 = Off, 1 = 700W, 2 = 1300W, 3 = 2000W
+    let power = (this.upperState ? 1 : 0) + (this.lowerState ? 2 : 0)
+    const onoff_response = await this.oAuth2Client.setDevicePoint(deviceId, { '517': power });
+    if (onoff_response.ok === false) {
+      throw new Error('Unable to Control device - Failed to power on/off');
+    }
+    this.log('Turned On/Off:', power)
+  }
+
   /**
    * onOAuth2Init is called when the device is initialized.
    */
@@ -43,15 +53,26 @@ class MyHoiaxDevice extends OAuth2Device {
       }, 1000*60*5) // Every 5 minute
       
 
+      const OnOffTrigger = this.homey.flow.getTriggerCard('the-water-heater-turned-onoff')
+      const OnOffAction  = this.homey.flow.getActionCard('turn-water-heater-onoff')
+
+      OnOffAction.registerRunListener(async (state) => {
+        this.upperState = (state['upper'] == "true")
+        this.lowerState = (state['lower'] == "true")
+        await this.setHeaterState(deviceId)
+      })
+
       // Register on/off handling
-      this.registerCapabilityListener('onoff', async (value) => {
-        // Value 0 = Off, 1 = 700W, 2 = 1300W, 3 = 2000W
-        let power = value ? 3 : 0
-        const onoff_response = await this.oAuth2Client.setDevicePoint(deviceId, { '517': power });
-        if (onoff_response.ok === false) {
-          throw new Error('Unable to Control device - Failed to power on/off');
-        }
-        this.log('Turned On/Off:', value)
+      this.registerMultipleCapabilityListener(['onoff.upper', 'onoff.lower'], async (value) => {
+        if (value['onoff.upper'] != undefined) { this.upperState = value['onoff.upper'] }
+        if (value['onoff.lower'] != undefined) { this.lowerState = value['onoff.lower'] }
+        const tokens = {
+          'upper-on': this.upperState,
+          'lower-on': this.lowerState
+        };
+        OnOffTrigger.trigger(tokens)
+          .catch(this.error);
+          await this.setHeaterState(deviceId)
       })
 
       // Register target temperature handling
@@ -65,15 +86,18 @@ class MyHoiaxDevice extends OAuth2Device {
     }
 
   async updateState(deviceId) {
-    const dev_points = await this.oAuth2Client.getDevicePoints(deviceId, '400,517,527,528');
-    this.setCapabilityValue('meter_power', dev_points[0].value) // 400 = EstimatedPower
-    if (dev_points[1].value == 0) { // 517 = Requested power
-      this.setCapabilityValue('onoff', false).catch(this.error)
-    } else {
-      this.setCapabilityValue('onoff', true).catch(this.error)
-    }
-    this.setCapabilityValue('target_temperature', dev_points[2].value) // 527 = Requested temperature
-    this.setCapabilityValue('measure_temperature', dev_points[3].value) // 528 = Measured temperature
+    const dev_points = await this.oAuth2Client.getDevicePoints(deviceId, '302,303,400,404,405,517,527,528');
+    this.setCapabilityValue('meter_power.in_tank', dev_points[0].value) // 302 = EnergyStored
+    this.setCapabilityValue('meter_power.accumulated', dev_points[1].value) // 303 = EnergyTotal
+    this.setCapabilityValue('measure_power', dev_points[2].value) // 400 = EstimatedPower
+    this.setCapabilityValue('measure_humidity.fill_level', dev_points[3].value) //404 = FillLevel
+    this.setCapabilityValue('measure_humidity.efficiency', dev_points[4].value) //405 = HeaterEfficiency
+    this.upperState = (dev_points[5].value & 1) == 1 // 517 = Requested power
+    this.lowerState = (dev_points[5].value & 2) == 2 // 517 = Requested power
+    this.setCapabilityValue('onoff.upper', this.upperState).catch(this.error)
+    this.setCapabilityValue('onoff.lower', this.lowerState).catch(this.error)
+    this.setCapabilityValue('target_temperature', dev_points[6].value) // 527 = Requested temperature
+    this.setCapabilityValue('measure_temperature', dev_points[7].value) // 528 = Measured temperature
   }
 
   async onOAuth2Deleted() {
